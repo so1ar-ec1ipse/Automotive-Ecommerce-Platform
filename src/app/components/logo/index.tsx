f375a388ad80bf30d4f3e88f8546cca9a6f99036
sinansubasi95 <sinan95@gmail.com>
2021-06-27 12:34:02 +0300

    Creating & Designing App Footer

--- src/app/components/logo/index.tsx
+++ src/app/components/logo/index.tsx
@@ -3,6 +3,12 @@ import styled from 'styled-components';
 import tw from 'twin.macro';
 
 import CarLogoImg from '../../../assets/images/car-logo.png';
+import CarLogoDarkImg from '../../../assets/images/car-logo-dark.png';
+
+interface ILogoProps {
+  color?: 'white' | 'dark';
+  bgColor?: 'white' | 'dark';
+}
 
 const LogoContainer = styled.div`
   ${tw`
@@ -18,7 +24,8 @@ const LogoText = styled.div`
         font-bold
         text-black
         m-1
-    `}
+        `}
+  ${({ color }: any) => (color === 'white' ? tw`text-white` : tw`text-black`)}
 `;
 
 const Image = styled.div`
@@ -34,13 +41,15 @@ const Image = styled.div`
   }
 `;
 
-export function Logo() {
+export function Logo(props: ILogoProps) {
+  const { color, bgColor } = props;
+
   return (
     <LogoContainer>
       <Image>
-        <img src={CarLogoImg} />
+        <img src={bgColor === 'dark' ? CarLogoDarkImg : CarLogoImg} />
       </Image>
-      <LogoText>Yourcar.</LogoText>
+      <LogoText color={color || 'dark'}>Yourcar.</LogoText>
     </LogoContainer>
   );
 }
