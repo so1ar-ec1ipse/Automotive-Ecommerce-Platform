953219b181d518b8d1ccc5052193d2cf6111c5a1
sinansubasi95 <sinan95@gmail.com>
2021-06-27 16:03:29 +0300

    Setting Up TypeORM with MySQL database on Nestjs

---
+++ nestjs-cars-app/src/database/database.module.ts
@@ -0,0 +1,14 @@
+import { Module } from '@nestjs/common';
+import { Connection } from 'typeorm';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+    imports: [TypeOrmModule.forRoot()],
+    exports: [TypeOrmModule]
+})
+
+export class DatabaseModule {
+    constructor(private connection: Connection) {
+        if(connection.isConnected) console.log("DB Connected Successfully");
+    }
+}
