39111d5745c4c6cf65cb2020172689d619750c81
sinansubasi95 <sinan95@gmail.com>
2021-06-27 16:10:24 +0300

    Setting Up Apollo GraphQL with Nestjs

--- nestjs-cars-app/src/app.module.ts
+++ nestjs-cars-app/src/app.module.ts
@@ -3,9 +3,14 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { DatabaseModule } from './database/database.module';
+import { GraphQLModule } from '@nestjs/graphql';
 
 @Module({
-  imports: [ConfigModule.forRoot(), DatabaseModule],
+  imports: [ConfigModule.forRoot(), DatabaseModule, GraphQLModule.forRoot({
+    playground: process.env.NODE_ENV === 'development' && true || false,
+    debug: process.env.NODE_ENV === 'development' && true || false,
+    autoSchemaFile: true,
+  })],
   controllers: [AppController],
   providers: [AppService],
 })
